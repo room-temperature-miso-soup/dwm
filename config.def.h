6a38d465e422c7037e0011f9c5d1ef6a1ef2b8aa
Room-Temperature-Miso-Soup <miguelarbis@gmail.com>
2025-07-16 21:21:27 -0400

    more class tags changes

--- config.def.h
+++ config.def.h
@@ -151,10 +151,10 @@ static const Rule rules[] = {
 	RULE(.class = "Gimp", .tags = 1 << 4)
   RULE(.class = "Brave-browser", .tags = 2, .switchtag = 1)
   RULE(.class = "wezterm", .tags = 1, .switchtag = 1)
-  RULE(.class = "discord", .tags = 4, .switchtag = 1)
-  RULE(.class = "protonvpn-app", .tags = 6, .switchtag = 1)
-  RULE(.class = "steam", .tags = 5, .switchtag = 1)
-  RULE(.class = "Counter Strike 2", .tags = 4, .switchtag = 1)};
+  RULE(.class = "discord", .tags = 5, .switchtag = 1)
+  RULE(.class = "Protonvpn-app", .tags = 6, .switchtag = 1)
+  RULE(.class = "steam", .tags = 6, .switchtag = 1)
+  RULE(.class = "cs2", .tags = 4, .switchtag = 1);
 
 /* Bar rules allow you to configure what is shown where on the bar, as well as
  * introducing your own bar modules.
