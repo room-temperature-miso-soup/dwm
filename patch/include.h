7ceff5b40fcfc449181e61dc8246c78f21697cf2
Walis User <miguelarbis@protonmail.com>
2025-07-17 00:19:04 +0000

    rebuilt

---
+++ patch/include.h
@@ -0,0 +1,26 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "dwmc.h"
+#include "focusfollowmouse.h"
+#include "killunsel.h"
+#include "restartsig.h"
+#include "sticky.h"
+#include "swallow.h"
+#include "vanitygaps.h"
+#include "xrdb.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
